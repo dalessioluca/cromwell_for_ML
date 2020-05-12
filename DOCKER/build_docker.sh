d58732258ccdc2a85e031c2ef2008ddd707e3709
dalessioluca <ldalessi@broadinstitute.org>
2020-05-12 11:43:11 -0400

    set up to work with multiple tags

---
+++ DOCKER/build_docker.sh
@@ -0,0 +1,6 @@
+#docker build --tag us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.1 ./TAG_0.0.1
+#docker push us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.1
+
+docker build --tag us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.2 ./TAG_0.0.2
+docker push us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.2
+
